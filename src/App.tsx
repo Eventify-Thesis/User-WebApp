82ca9e37f1ad701266097e050de2663afe66b8cd
Hoang Bui <hoang.bui@c0x12c.com>
2025-02-13 22:17:16 +0700

    feat: Set up project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { AppRouter } from "./components/router/AppRouter";
 import { useLanguage } from "./hooks/useLanguage";
 
 function App() {
-  const {language} = useLanguage();
-
-
-  return <AppRouter>
+  const { language } = useLanguage();
+  return <AppRouter />;
 }
 
 export default App;
