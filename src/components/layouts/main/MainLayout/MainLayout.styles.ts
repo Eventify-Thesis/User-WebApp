0b5292d6dde88101e6de2640b725987e5a7354d2
Hoang Bui <hoang.bui@c0x12c.com>
2025-02-14 17:35:23 +0700

    revert changes

--- src/components/layouts/main/MainLayout/MainLayout.styles.ts
+++ src/components/layouts/main/MainLayout/MainLayout.styles.ts
@@ -6,4 +6,12 @@ export const LayoutMaster = styled(BaseLayout)`
   height: 100vh;
 `;
 
-export const LayoutMain = styled(BaseLayout)``;
+export const LayoutMain = styled(BaseLayout)`
+  @media only screen and ${media.md} {
+    margin-left: 80px;
+  }
+
+  @media only screen and ${media.xl} {
+    margin-left: unset;
+  }
+`;
